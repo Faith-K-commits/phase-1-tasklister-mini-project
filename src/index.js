239fa78858682fe7e5b33eb3d02b8bb69e3f9b43
Faith Kariuki <tcwamboi@gmail.com>
2024-07-05 19:19:45 +0300

    Complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+
+    
+    const taskDescription = document.getElementById('new-task-description').value;
+
+    
+    const newTask = document.createElement('li');
+    newTask.textContent = taskDescription;
+    newTask.innerHTML += '<button data-description="taskDescription">X</button>'; 
+    newTask.querySelector('button').addEventListener('click', handleDelete);
+
+    
+    taskList.appendChild(newTask);
+
+    
+    document.getElementById('new-task-description').value = '';
+  });
+
+  const handleDelete = (e) => {
+    e.target.parentElement.remove();
+  };
 });
